10f8da0404118efe6187cd885f932c5e2a6ed920
Anik Paul Gomes <gomesap29@gmail.com>
2020-04-05 17:30:09 -0500

    few tweaks

--- SourceCode/MergeSort/src/main/scala/MergeSort.scala
+++ SourceCode/MergeSort/src/main/scala/MergeSort.scala
@@ -8,24 +8,21 @@ object MergeSort {
     println(mergeSort(List(38,27,43,3,9,82,10)))
   }
 
-  def mergeSort(numbers: List[Int]): List[Int] = {
-    val halfLength = numbers.length / 2
-    if(halfLength == 0) {
-      numbers
-    }
-    else{
-      val (left, right) = numbers splitAt(halfLength)
-      merge(mergeSort(left), mergeSort(right))
-    }
+  def mergeSort(lst: List[Int]): List[Int] = lst match  {
+    case Nil => lst
+    case h::Nil => lst
+    case _ =>
+      val (l1, l2) = lst.splitAt(lst.length/2)
+      merge(mergeSort(l1), mergeSort(l2))
   }
 
-  def merge(firstHalf: List[Int], secondHalf: List[Int]) : List[Int] =
-    (firstHalf, secondHalf) match {
-      case(Nil, secondHalf) => secondHalf
-      case(firstHalf, Nil) => firstHalf
-      case(x :: fh1, y:: sh1) =>
-        if( x < y) x:: merge(fh1, secondHalf)
-        else y :: merge(firstHalf, sh1)
+  def merge(l1: List[Int], l2: List[Int]) : List[Int] =
+    (l1, l2) match {
+      case(Nil, _) => l2
+      case(_, Nil) => l1
+      case(h1 :: t1, h2:: t2) =>
+        if( h1 < h2) h1::merge(t1, l2)
+        else h2::merge(l1,t2 )
     }
 
 }
